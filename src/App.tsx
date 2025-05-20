0d07283bc5ad4235e12d7545ed7c2de78f7cdbe5
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-05-20 15:31:56 +0100

    simple header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Button } from '@heroui/react'
-import React from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Layout from './Layout'
+import Profile from './pages/profile/Profile'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import ForgetPassword from './pages/auth/ForgetPassword'
+import Show from './pages/conversation/Show'
 const App = () => {
   return (
-    <div>
-      <Button>test</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='conversations/show/:id' element={<Show />} />
+          <Route path='profile/:id' element={<Profile />} />
+        </Route>
+
+
+        <Route path='/auth' element={<Layout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='forget-password' element={<ForgetPassword />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HeroUIProvider } from '@heroui/react';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,12 +14,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HeroUIProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HeroUIProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
